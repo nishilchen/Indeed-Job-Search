e8a60a1fd493e23c8dc1fe2788f3b1fe7f4443ff
nishilchen <nishil0510@gmail.com>
2018-07-20 11:49:51 -0400

    jobapp.py

--- jobapp.py
+++ jobapp.py
@@ -2,17 +2,17 @@
 """
 Created on Thu Jul 19 21:26:35 2018
 
-@author: nishi
+@author: nishil
 """
-#%%
+#%% Import Packages
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
-
+#%%
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-
+#%%
 class jobpage:
     def __init__(self, url):
         self.url = url
@@ -29,13 +29,17 @@ class jobpage:
         self.soup = BeautifulSoup(html, "html.parser")
         return self.soup
    
-    def CheckWordIn(self,word,tag):
-        soup = self.getsoup()
-        return [a for a in soup.findAll(tag) if word in repr(a)]
+# =============================================================================
+#     def CheckWordIn(self,word,tag):
+#         soup = self.getsoup()
+#         return [a for a in soup.findAll(tag) if word in repr(a)]
+# =============================================================================
     
     
 #%% Indeed Specific
+
 class Indeedjoblistpage(jobpage):
+    src = 'https://www.indeed.com'
     def findjobmap(self):
         soup = self.getsoup()
         return [a for a in soup.findAll('a') if 'jobmap' in repr(a)]
@@ -44,59 +48,68 @@ class Indeedjoblistpage(jobpage):
     def geturl(self):
         jobmap = self.findjobmap()
         #jobmap = self.CheckWordIn('jobmap')
-        src = 'https://www.indeed.com'
         result = []
         for j in jobmap:
             if '/rc/clk?' in j['href']:
-                result.append(src + '/viewjob' + j['href'][7:])
+                result.append(self.src + '/viewjob' + j['href'][7:])
             else:
-                result.append(src + j['href'])
+                result.append(self.src + j['href'])
         return result
 
+    def IndeedNextPage(self,url):
+        if '&start' in self.url:
+            strings = self.url.split('&start=')
+            page = '&start=' + str(int(strings.pop()) + 10)
+            self.url = strings[0] + page
+            return self.url
+        else:
+            self.url = self.url + '&start=10'
+            return self.url
 
-
-
+    def geturls(self,pages):
+        results = []
+        for i in range(pages):
+            results.extend(self.geturl())
+            self.url = self.IndeedNextPage(self.url)
+        return results
 #%%
-url = 'https://www.indeed.com/jobs?q=data+scientist&l=Washington%2C+DC'
+def IndeedSearch(what, where):
+    src = 'https://www.indeed.com'
+    return src + '/' + 'jobs?q=' + '+'.join(what.split(' ')) + '&l=' + '%2C+'.join(where.split(', '))
+#%%
+# =============================================================================
+# for u in urllist:
+#     try:
+#         job = jobpage(u)
+#         job.getsoup()
+#         if 'US citizen' in repr(job.soup):
+#             urllist.remove(u)
+#     except:
+#         print(u)
+# =============================================================================
+def CheckWordsIn(strings,urllist):
+    urlresult = []
+    for u in urllist:
+        try:
+            job = jobpage(u)
+            job.getsoup()
+            if any([s in repr(job.soup) for s in strings]):
+                continue
+            else:
+                urlresult.append(u)
+        except:
+            pass
+    return urlresult
+#%% Main
+what, where = 'Statistics','Arlington, VA'
+num_pages = 3
+
+url = IndeedSearch(what, where)
 joblist = Indeedjoblistpage(url)
-urllist = joblist.geturl()
+urllist = joblist.geturls(num_pages)
+len(urllist)
 
-for u in urllist:
-    try:
-        job = jobpage(u)
-        job.getsoup()
-        if 'US citizen' in repr(job.soup):
-            urllist.remove(u)
-    except:
-        print(u)
-    
-
-
-
-
-
-
-#%%
-# =============================================================================
-# def gethtml(url):
-#     html = urllib.request.urlopen(url, context = ctx).read()
-#     return html
-# 
-# 
-# def getsoup(html):
-#     soup = BeautifulSoup(html, "html.parser")
-#     return soup
-# =============================================================================
-
-#%%
-        
-        
-        
-        
-        
-#def CheckWordIn:
-#def CheckWordNotIn:
-        
-        
-        
-        
+unwanted = ['US citizen', "TS", "SCI", "DoD", "TOP SECRET", "Top secret"]
+urlresult = CheckWordsIn(unwanted,urllist)
+urlresult
+len(urlresult)
